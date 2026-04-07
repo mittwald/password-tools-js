1fb398db8655c786c72209dec8e71d48eb1ba8c7
Marco Falkenberg <m.falkenberg@mittwald.de>
2026-04-07 16:37:59 +0200

    fix: drop VError because it is only supported by Node runtime

--- src/errors.ts
+++ src/errors.ts
@@ -1,60 +1,51 @@
-import VError from "verror";
 import type { Policy } from "./policy/Policy.js";
 
-export class PolicyParseError extends VError {
+export class PolicyParseError extends Error {
   public constructor(cause?: string) {
-    super(
-      {
-        name: "PolicyParseError",
-        info: {
-          cause,
-        },
-      },
-      "Policy could not be parsed",
-    );
+    super("Policy could not be parsed", {
+      cause,
+    });
   }
 }
+PolicyParseError.prototype.name = "PolicyParseError";
 
-export class PolicyNotFoundError extends VError {
+export class PolicyNotFoundError extends Error {
+  public readonly policy: string;
+
   public constructor(policy: string, cause?: Error) {
-    super(
-      {
-        name: "PolicyNotFoundError",
-        info: {
-          policy,
-        },
-        cause,
-      },
-      `Policy "${policy}" not found`,
-    );
+    super(`Policy "${policy}" not found`, {
+      cause,
+    });
+    this.policy = policy;
   }
 }
+PolicyNotFoundError.prototype.name = "PolicyNotFoundError";
 
-export class PasswordGenerationError extends VError {
+export class PasswordGenerationError extends Error {
+  public readonly policy: string;
+  public readonly rejectedPasswords?: string[];
+  public readonly timeout: number;
+
   public constructor(
     policy: Policy,
     timeout: number,
     rejectedPasswords?: string[],
   ) {
     super(
-      {
-        name: "PasswordGenerationError",
-        info: {
-          policy: JSON.stringify(policy),
-          timeout,
-          triedPasswords: rejectedPasswords,
-        },
-      },
       "Exceeded timeout while trying to generate a password. Does the policy contradict itself?",
     );
+    this.policy = JSON.stringify(policy);
+    this.timeout = timeout;
+    this.rejectedPasswords = rejectedPasswords;
   }
 }
+PasswordGenerationError.prototype.name = "PasswordGenerationError";
 
-export class CryptographicInsecurityError extends VError {
+export class CryptographicInsecurityError extends Error {
   public constructor() {
     super(
-      { name: "CryptographicInsecurityError" },
       "No cryptographically secure generator available, abort password-generation",
     );
   }
 }
+CryptographicInsecurityError.prototype.name = "CryptographicInsecurityError";
