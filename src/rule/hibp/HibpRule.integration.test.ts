04801ee08090636a18cad74c8591a88d74c2091f
Marco Rieger <m.rieger@mittwald.de>
2026-07-06 09:02:22 +0200

    add test

---
+++ src/rule/hibp/HibpRule.integration.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+import { HibpRule } from "./HibpRule.js";
+
+describe(
+  `${HibpRule.name}.integration.axios`,
+  () => {
+    test("will obey succeedOnError when run in timeout", async () => {
+      expect(
+        (
+          await new HibpRule({
+            // not reachable url
+            endpointUrl: "http://10.255.255.1/",
+            willSucceedOnError: true,
+          }).validate("123")
+        ).isValid,
+      ).toBeTruthy();
+    });
+  },
+  { timeout: 10000 },
+);
